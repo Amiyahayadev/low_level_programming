0dee08a5e20923a5e7a142f465482bba4e803fa6
Amiyahayadev <ameenahyahayab@gmail.com>
2023-02-16 18:38:52 -0800

    size in bytes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-
-/*
+/**
+ * Return: 0
  * Program to calculate size each varible type will 
  * occpy in memory.
  */
@@ -12,11 +12,11 @@ int main(void)
 	long long int j;
         float f;
 
-        printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(c));
-        printf("Size of an int: %lu byte(s)\n",(unsigned long)sizeof(i));
-        printf("Size of a long int:: %lu byte(s)\n",(unsigned long)sizeof(n));
-        printf("Size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(j));
-        printf("Size of a float: %lu byte(s)\n",(unsigned long)sizeof(f));
+        printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+        printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+        printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(n));
+        printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(j));
+        printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
         return (0);
 }
 
