f52c6d5c1315f10d3dd1f44506c6fc77b54e8fc3
Amiyahayadev <ameenahyahayab@gmail.com>
2023-02-16 19:15:10 -0800

    betty

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,15 @@
 #include <stdio.h>
 /**
- * Return: 0
- * Program to calculate size each varible type will 
- * occpy in memory.
+ * how to get byte
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-char c;int i;
-long int n;
-long long int j;
-float f;
-        printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
-        printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
-        printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(n));
-        printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(j));
-        printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-        return (0);
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
+	printf("Size of an int: %i byte(s)\n", sizeof(int));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %i byte(s)\n", sizeof(float));
+	return (0);
 }
-
