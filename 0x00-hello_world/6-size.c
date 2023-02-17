ff6055c8cadda595cb33ea1e4813dede278f8e0c
Amiyahayadev <ameenahyahayab@gmail.com>
2023-02-16 17:59:26 -0800

    sixies

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/*
+ * Program to calculate size each varible type will 
+ * occpy in memory.
+ */
+int main(void)
+{
+	char c;
+        int i;
+	(long int) li;
+	(long long int) lli;
+        float f;
+
+        printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(c));
+        printf("Size of an int: %lu byte(s)\n",(unsigned long)sizeof(i));
+        printf("Size of a long int:: %lu byte(s)\n",(unsigned long)sizeof(li));
+        printf("Size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(lli));
+        printf("Size of a float: %lu byte(s)\n",(unsigned long)sizeof(if));
+        return 0;
+}
+
