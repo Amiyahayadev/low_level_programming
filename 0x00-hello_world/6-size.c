8f9cdfff7784fae50c7d9666a8ca1485f9dac12c
Amiyahayadev <ameenahyahayab@gmail.com>
2023-02-16 18:43:12 -0800

    no spaces at start

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,12 +6,10 @@
  */
 int main(void)
 {
-	char c;
-        int i;
-	long int n;
-	long long int j;
-        float f;
-
+char c;int i;
+long int n;
+long long int j;
+float f;
         printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
         printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
         printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(n));
