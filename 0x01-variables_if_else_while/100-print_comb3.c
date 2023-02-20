0197270160c598b8abe124ee24b781880a062ac9
Amiyahayadev <ameenahyahayab@gmail.com>
2023-02-20 11:29:22 -0800

    combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description - all signle digit combinations
+ * Return: Always 0 if (success)
+ */
+int main(void)
+{
+	int i, j;
+
+	i = 0;
+	while (i < 10)
+	{
+		j = i + 1;
+		while (j < 10)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
