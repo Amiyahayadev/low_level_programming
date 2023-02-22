4e34bcc02e945006a86786401206a56a91be6901
Amiyahayadev <ameenahyahayab@gmail.com>
2023-02-21 16:31:20 -0800

    initial

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,17 +7,16 @@
  * Description - Print string
  * Return: Always 0 if (success)
  */
-int _putchar(char c);
-int main(void)
-{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	putchar('\n');
+int main(void){
+	char s[] = "_putchar";
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	_putchar('\n');
+
 	return (0);
 }
