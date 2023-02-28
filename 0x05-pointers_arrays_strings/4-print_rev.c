f872ce867fb0666a7e28e72b018283fb02690129
Amiyahayadev <ameenahyahayab@gmail.com>
2023-02-28 11:04:59 -0800

    revers

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,19 +8,13 @@
  */
 void print_rev(char *s)
 {
-	int len = 0;
+	int len;
 
-	char *p = s;
+	int i;
 
-	/* Find the length of the string */
-	while (*p != '\0')
-	{
-		len++;
-		p++;
-	}
-	/* Print the string in reverse */
-	for (int i = len - 1; i >= 0; i--)
-		_putchar(s[i]);
+	len = _strlen(s);
+	for (i = len - 1; i >= 0; i--)
+		_putchar(*(s + i));
 
 	_putchar('\n');
 }
