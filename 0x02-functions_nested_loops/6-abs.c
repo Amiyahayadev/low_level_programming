f688e882256b6352cd21b7f0eb9eed09600ccc89
Amiyahayadev <ameenahyahayab@gmail.com>
2023-03-04 11:17:42 -0800

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,14 +1,9 @@
 #include "main.h"
-#include <stdlib.h>
-
 /**
- * _abs - get the absoluite of a number
- * Description: if a number is negative
- * it makes it positive
- * @n: a number to be entered by user
- * Return: either 0 or a positive number
+ *
+ *
  */
-int abs(int n)
+int _abs(int)
 {
-	return (abs(n));
+
 }
