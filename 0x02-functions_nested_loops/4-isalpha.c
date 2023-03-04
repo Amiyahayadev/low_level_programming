eb7e2e616254d4f2493d18e2c05cce71b96142c8
Amiyahayadev <ameenahyahayab@gmail.com>
2023-03-04 12:03:49 -0800

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,14 +5,14 @@
  * Description: Get an entry from user and prints
  * 1 in case it's a letter and 0 in case it's not
  * @c: a variable to be entered by user
- * 
+ *
  * Return: 0 or 1
  */
 int _isalpha(char c)
 {
 	if (c >= 'a' && c <= 'z')
 		return (1);
-	
+
 	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
