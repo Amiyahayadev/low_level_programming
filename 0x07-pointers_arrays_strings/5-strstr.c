6a8d8418aa28d443639b94123ac3746eca798419
Amiyahayadev <ameenahyahayab@gmail.com>
2023-03-07 12:47:17 -0800

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,5 +11,17 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i;
+	for (; *haystack; haystack++)
+	{
+		char *h = haystack;
+		char *n = needle;
+			while (*h == *n && *n != '\0')
+			{
+				h++;
+				n++;
+			}
+			if (*n == '\0')
+				return (haystack);
+	}
+	return (0);
 }
