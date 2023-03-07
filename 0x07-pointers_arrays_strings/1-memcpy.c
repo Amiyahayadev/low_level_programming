cb46feb9ac1eb987e55b56b8cf0cbdb0f7fff3ba
Ms Amina Yahaya Baba <ameenahyahayab@gmail.com>
2023-03-07 13:21:01 +0100

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -13,11 +13,9 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i, j;
 
-	j = 0;
 	for (i = 0; i < n; i++)
 	{
-		dest[j] = src[i];
-		j++;
+		dest[i] = src[i];
 	}
 	return (dest);
 }
