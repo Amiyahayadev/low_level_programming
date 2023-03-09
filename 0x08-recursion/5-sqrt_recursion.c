62bc688c43c8ee1183e5db6066f0750afae2c04a
Amiyahayadev <ameenahyahayab@gmail.com>
2023-03-09 12:33:37 -0800

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,25 +1,24 @@
 #include "main.h"
 
+int sqrt_helper(int n, int i);
+
 /**
- * _sqrt_recursion - Frinds square
- * @n - number to find
+ * _sqrt_recursion - returns the square of n
+ * @n - number to evaluate
  *
  * Return: the natural square root of n or -1
  */
-int sqrt_helper(int n, int i);
-
 int _sqrt_recursion(int n)
 {
-	return sqrt_helper();
+	return (sqrt_helper(n, 0));
 }
 
 /**
- * sqrt_helper - checks if the square of the current number
- * i is greater than the given number n
- * @i: increment
+ * sqrt_helper - checks if number has a natural square root
+ * @i: variable data(Sq root)
  * @n: number
  *
- * Return: square root of  natural square root
+ * Return: i, natural square root of a number
  */
 
 int sqrt_helper(int n, int i)
@@ -27,6 +26,6 @@ int sqrt_helper(int n, int i)
 	if (i * i > n)
 		return (-1);
 	if (i * i == n)
-		return (0);
-	return (sqrt_helper(n, i + 1))
+		return (i);
+	return (sqrt_helper(n, i + 1));
 }
