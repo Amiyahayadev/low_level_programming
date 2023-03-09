fddd81e6b13614d142be26d21d8f39d09d96a44d
Amiyahayadev <ameenahyahayab@gmail.com>
2023-03-08 19:41:56 -0800

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - computes x^y = x * x^(y-1)
+ * @x: base value whose power is to be calculated
+ * @y: the power value
+ *
+ * Return: result or power, 1 or -1
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y-1));
+}
