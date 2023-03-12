fb7d5912eaedf193b013345eea2f05daeadd22c9
Amiyahayadev <ameenahyahayab@gmail.com>
2023-03-11 16:53:11 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,72 +2,19 @@
 
 /**
  * main - program that prints a string to std error
- *
- * Description - Prints specific string
- *
+ * Description - @msg is Pointer variable that holds string
+ * write function is used to write string to the standard error,
+ * argument is 2, represents the file descriptor for standard error.
  * Return: 1
  */
 int main(void)
 {
-	putchar('a');
-	putchar('n');
-	putchar('d');
-	putchar(' ');
-	putchar('t');
-	putchar('h');
-	putchar('a');
-	putchar('t');
-	putchar(' ');
-	putchar('p');
-	putchar('i');
-	putchar('e');
-	putchar('c');
-	putchar('e');
-	putchar(' ');
-	putchar('o');
-	putchar('f');
-	putchar(' ');
-	putchar('a');
-	putchar('r');
-	putchar('t');
-	putchar(' ');
-	putchar('i');
-	putchar('s');
-	putchar(' ');
-	putchar('u');
-	putchar('s');
-	putchar('e');
-	putchar('f');
-	putchar('u');
-	putchar('l');
-	putchar(' ');
-	putchar('"');
-	putchar(' ');
-	putchar('-');
-	putchar(' ');
-	putchar('D');
-	putchar('o');
-	putchar('r');
-	putchar('a');
-	putchar(' ');
-	putchar('K');
-	putchar('o');
-	putchar('r');
-	putchar('p');
-	putchar('a');
-	putchar('r');
-	putchar(',');
-	putchar(' ');
-	putchar('2');
-	putchar('0');
-	putchar('1');
-	putchar('5');
-	putchar('-');
-	putchar('1');
-	putchar('0');
-	putchar('-');
-	putchar('1');
-	putchar('9');
-	putchar('\n');
+	char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+
+	int i;
+
+	for (i = 0; msg[i] != '\0'; i++)
+
+	write(2, msg, i);
 	return (1);
 }
