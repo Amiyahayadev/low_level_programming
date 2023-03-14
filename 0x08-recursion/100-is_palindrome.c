6c34c797787461a6886db5f065c371da6d75db88
Amiyahayadev <ameenahyahayab@gmail.com>
2023-03-14 06:50:47 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -12,7 +12,7 @@ int is_palindrome_helper(char *s, int start, int end);
 int is_palindrome(char *s)
 {
 		int len;
-		
+
 		len = strlen(s);
 		return (is_palindrome_helper(s, 0, len - 1));
 }
@@ -25,13 +25,13 @@ int is_palindrome(char *s)
  *
  * Return:  1 if a string is a palindrome and 0 if not.
  */
- 
- int is_palindrome_helper(char *s, int start, int end)
- {
- 	if (start >= end)
+
+int is_palindrome_helper(char *s, int start, int end)
+{
+	if (start >= end)
 		return (1);
 	else if (s[start] != s[end])
 		return (0);
 	else
 		return (is_palindrome_helper(s, start + 1, end - 1));
- }
+}
