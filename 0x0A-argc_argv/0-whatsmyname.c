c52d5a190aab542db36606b94fc3389834caae39
Amiyahayadev <ameenahyahayab@gmail.com>
2023-03-15 10:33:43 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - program that prints it's name
+* @argc: count to all arguments
+* @argv: pointer to an array of pointers to strings(the arguments)
+* Return: 0 on success
+*/
+int main(int argc, char **argv)
+{
+	int i;
+
+	if (argc < 1)
+			
+}
