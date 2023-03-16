f54df9fe31f6617e3428215958e7b7dbd5e2f321
Amiyahayadev <ameenahyahayab@gmail.com>
2023-03-15 17:40:31 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -19,7 +19,7 @@ int main(int argc, char **argv)
 		return (1);
 	}
 	for (i = 1; argc < 4 && i < argc; i++)
-		mulRes *= atoi(agrv[i]);
+		mulRes *= atoi(argv[i]);
 
 	printf("%d\n", mulRes);
 	return (0);
