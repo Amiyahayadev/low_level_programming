c12ecfc8cdc4cb11476ffb3edf145852320aa354
Amiyahayadev <ameenahyahayab@gmail.com>
2023-03-16 20:16:22 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,7 +19,6 @@ int _strlen(char *s)
 	}
 	return (len);
 }
-
 /**
 * *string_nconcat - program concatenates two strings but copies n bytes
 * @s1: first string to add
@@ -27,45 +26,39 @@ int _strlen(char *s)
 * @n: first n bytes to be copied from s2
 * Return: pointer to a newly allocated space in memory
 */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	/*declare pointer to newly allocated block*/
 	char *s3n;
-	
-	int l1, l2, l3;
 
-	int r, j;
-	
+	unsigned int l1, l2, l3, i, j;
+
 	/*if NULL is passed, treat it as an empty string*/
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	ln = 0;	
-	while (s2[ln] < n + 1)
-		ln++;
 
 	l1 = _strlen(s1);
 	l2 = _strlen(s2);
-	l3n = l1 + ln + 1;
-	l3 = l1 + l2 + 1;
 
 	/*If n is >= the length of s2 then use the entire string s2*/
 	if (n >= l2)
-		s3n = malloc(sizeof *s3n * l3);
-	else
-		s3n = malloc(sizeof *s3n * l3n);
+		n = l2;
+
+	l3 = l1 + n + 1;
+	s3n = malloc(sizeof *s3n * l3);
 
 	if (s3n == NULL)
 	{
 		return (NULL);
 	}
-	for (r = 0; r < l1; r++)
+	for (i = 0; i < l1; i++)
 		s3n[r] = s1[r];
-	for (j = 0; j < n + 1; j++)
-		s3n[r + j] = s2[j];
-	
-	s3n[r + j] = '\0';
+	for (j = 0; j < n; j++)
+		s3n[i + j] = s2[j];
+
+	s3n[i + j] = '\0';
+
 	return (s3n);
 }
