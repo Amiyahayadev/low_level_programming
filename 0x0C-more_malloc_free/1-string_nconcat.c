6f065c1eea3b061be0998590a36b74c7420a7a63
Amiyahayadev <ameenahyahayab@gmail.com>
2023-03-16 20:18:48 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -54,7 +54,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	}
 	for (i = 0; i < l1; i++)
-		s3n[r] = s1[r];
+		s3n[i] = s1[i];
 	for (j = 0; j < n; j++)
 		s3n[i + j] = s2[j];
 
