267d8127101a71d368d64fa489cafe79955a2ee2
Amiyahayadev <ameenahyahayab@gmail.com>
2023-03-17 05:04:21 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -25,13 +25,15 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	return (arr);
 }
 
-/** *_memset - fill a block of memory with a particular value
+/**
+ * *_memset - fill a block of memory with a particular value
  * @s: Starting address of memory to be filled
  * @b: Value to be filled
  * @n: Number of bytes to be filled starting from ptr to be filled
  *
  * Return: a pointer to the memory area(s)
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
