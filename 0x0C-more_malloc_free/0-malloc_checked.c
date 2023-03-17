24f9e648a41ed9eb7acd8c926ef5fd69da295533
Amiyahayadev <ameenahyahayab@gmail.com>
2023-03-16 17:37:49 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,12 +4,17 @@
 /**
 * *malloc_checked - program that that allocates memory using malloc
 * @b: size of bytes to allocate
-* Return: pointer to the address of first element
+* Return: pointer to the address of allocated memory
+* if malloc fails,function exits with a status value of 98
 */
 void *malloc_checked(unsigned int b)
 {
-	if(malloc(b) == NULL)
+	void *p = malloc(b);
+
+	if(p == NULL)
 	{
 		exit(98);
 	}
+
+	return (p);
 }
