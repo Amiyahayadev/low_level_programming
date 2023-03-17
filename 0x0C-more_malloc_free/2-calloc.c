0a84269bde0e5f103fef598160e110de315636bb
Amiyahayadev <ameenahyahayab@gmail.com>
2023-03-17 13:25:59 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,6 +2,25 @@
 #include <stdlib.h>
 
 /**
+ * *_memset - fill a block of memory with a particular value
+ * @s: Starting address of memory to be filled
+ * @b: Value to be filled
+ * @n: Number of bytes to be filled starting from ptr to be filled
+ *
+ * Return: a pointer to the memory area(s)
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
+
+/**
 * *_calloc - function that allocates memory for an array like calloc
 * @nmemb: number of elements of the array of int data type
 * @size: number of bytes each element
@@ -18,29 +37,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	arr = malloc(nmemb * size);
 	if (arr == NULL)
 		return (NULL);
-	/*proceed to initialize memory with 0*/
-	/*pointer of type void* can't be used in arithmetic*/
-	_memset(arr, 0, nmemb * size);
+
+	_memset(arr, 0, (nmemb * size));
 
 	return (arr);
-}
-
-/**
- * *_memset - fill a block of memory with a particular value
- * @s: Starting address of memory to be filled
- * @b: Value to be filled
- * @n: Number of bytes to be filled starting from ptr to be filled
- *
- * Return: a pointer to the memory area(s)
- */
-
-char *_memset(char *s, char b, unsigned int n)
-{
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-	{
-		((char *)s)[i] = b;
-	}
-	return (s);
 }
