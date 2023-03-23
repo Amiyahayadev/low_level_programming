6f538e3a00d81bac512ce7c66adf0606d2c00527
Amiyahayadev <ameenahyahayab@gmail.com>
2023-03-23 14:12:51 -0700

    added documetation for struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,12 @@
+/**
+ * struct dog - new type for a dog struct
+ * @name: pointer to name string
+ * @age: variable of float type
+ * @owner: pointer to owner string
+ *
+ * Description: Longer description
+ */
+
 #ifndef DOG_H
 #define DOG_H
 
