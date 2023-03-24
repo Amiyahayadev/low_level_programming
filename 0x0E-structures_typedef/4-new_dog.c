faff5902837fa74ea4efc9829c585aeba0d74e49
Amiyahayadev <ameenahyahayab@gmail.com>
2023-03-23 17:03:23 -0700

    function creates a new struct dog variable

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+* *new_dog - creates a new dog variable
+* @name: pointer to member of struct dog that holds string name
+* @age: second member of struct, variable of type float
+* @owner: pointer to third member of struct dog that holds string owner
+* Return: pointer to the new dog_t variable/object or NULL if failed
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *my_dog;
+
+	my_dog = malloc(sizeof(struct dog));
+	if (my_dog == NULL)
+		return (NULL);
+
+	my_dog->name = strdup(name);
+	my_dog->age = age;
+	my_dog->owner = strdup(owner);
+
+	return (my_dog);
+}
