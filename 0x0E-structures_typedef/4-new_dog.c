aadb29b7903d7ccf0dd3efcf0dd29b345a7abf20
Amiyahayadev <ameenahyahayab@gmail.com>
2023-03-23 18:12:06 -0700

    function creates a new struct dog object

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -19,10 +19,21 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 
 	my_dog->name = strdup(name) != NULL ? strdup(name) : NULL;
+	if (strdup(name) == NULL)
+	{
+		free(my_dog);
+		return (NULL);
+	}
 
 	my_dog->age = age;
 
 	my_dog->owner = strdup(owner) != NULL ? strdup(owner) : NULL;
+	if (strdup(owner) == NULL)
+	{
+		free(my_dog->name);
+		free(my_dog);
+		return (NULL);
+	}
 
 	return (my_dog);
 }
