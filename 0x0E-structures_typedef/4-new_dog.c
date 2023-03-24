95d64de3b2a8201a7cffd6c81b00e95a3cee5f76
Amiyahayadev <ameenahyahayab@gmail.com>
2023-03-23 18:02:28 -0700

    function creates new struct vairable

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,34 +12,17 @@
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-
-	if (name == NULL || owner == NULL)
-	{
-		return (NULL);
-	}
-
 	dog_t *my_dog;
 
 	my_dog = malloc(sizeof(struct dog));
 	if (my_dog == NULL)
 		return (NULL);
 
-	my_dog->name = strdup(name);
-	if (my_dog->name ==  NULL)
-	{
-		free(my_dog);
-		return (NULL);
-	}
+	my_dog->name = strdup(name) != NULL ? strdup(name) : NULL;
 
 	my_dog->age = age;
 
-	my_dog->owner = strdup(owner);
-	if (my_dog->owner == NULL)
-	{
-		free(my_dog->name);
-		free(my_dog);
-		return (NULL);
-	}
+	my_dog->owner = strdup(owner) != NULL ? strdup(owner) : NULL;
 
 	return (my_dog);
 }
