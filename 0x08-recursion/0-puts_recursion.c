e764102ae70a071eebe09f1e60e84e0f48f94cb1
Amiyahayadev <ameenahyahayab@gmail.com>
2023-06-08 03:44:22 -0700

    definition of function that prints a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,20 @@
-#include <stdio.h>
+#include "main.h"
+
 /**
- * 
- *
+ * _puts_recursion - prints a string using recursion
+ * @s: pointer to string
+ * Return: nothing
  */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
