f8cb23efcfa2eb61364ad9602e6f0e8046431249
Amiyahayadev <ameenahyahayab@gmail.com>
2023-06-09 17:32:51 -0700

    Program that counts all arguments passed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point of program
+ * @argc: count of all arguments
+ * @argv: the string arguments passed from command line
+ * Return: 0 on SUCCESS
+ */
+int main(int argc, char __attribute__ ((unused)) *argv[])
+{
+	/* since no of arguments passed is given by argc */
+	printf("%d", argc);
+	printf("\n");
+	return (0);
+}
+
