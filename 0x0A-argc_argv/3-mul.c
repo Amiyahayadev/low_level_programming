0794b32735cdd210bb21cdcec3c91252b879ec36
Ms Amina Yahaya Baba <ameenahyahayab@gmail.com>
2023-06-10 04:26:32 +0100

    Update integers to store arguments

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,17 +10,18 @@
  */
 int main(int argc, char *argv[])
 {
-	int mul = 0;
+	int mul, arg1, arg2;
 
 	if (argc != 3)
 	{
 		printf("ERROR\n");
 		return (1);
 	}
-	else
-	{
-		mul = atoi(argv[1]) * atoi(argv[2]);
-		printf("%d\n", mul);
-	}
+	
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[2]);
+	mul = (arg1 * arg2);
+	
+	printf("%d\n", mul);
 	return (0);
 }
