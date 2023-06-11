346832c0bd6bc5038df556f040c234ec93372813
Amiyahayadev <ameenahyahayab@gmail.com>
2023-06-10 20:22:17 -0700

    update code for error message

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,18 +10,17 @@
  */
 int main(int argc, char *argv[])
 {
-	int mul, arg1, arg2;
+	int mul = 0;
 
 	if (argc != 3)
 	{
-		printf("ERROR\n");
+		printf("Error\n");
 		return (1);
 	}
-	
-	arg1 = atoi(argv[1]);
-	arg2 = atoi(argv[2]);
-	mul = (arg1 * arg2);
-	
-	printf("%d\n", mul);
+	else
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
+	}
 	return (0);
 }
