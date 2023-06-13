46491b930a1069cfe3a828226fc7cbfcc4e64951
Amiyahayadev <ameenahyahayab@gmail.com>
2023-06-13 01:39:51 -0700

    modified atoi() and re-created archive

---
+++ 0x09-static_libraries/c_files/100-atoi.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _atoi - function that converts text to integer
+ * @s: pointer tothe text to be converted
+ * Return: converted interger
+ */
+int _atoi(char *s)
+{
+
+}
