dd04aa59e592429dcee91db0b1bf97ac20964915
Amiyahayadev <ameenahyahayab@gmail.com>
2023-06-14 11:18:18 -0700

    function definition for str-concat()

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - this function concatenates two strings into
+ * a new memory location
+ * @s1: first string
+ * @s2: second string
+ * Return: pointer to the concatenated string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int i, j;
+
+	char *nstr;
+
+	unsigned int ln1, ln2, ln3;
+
+	ln1 = _strlen(s1);
+	ln2 = _strlen(s2);
+	ln3 = ln1 + ln2 + 1;
+
+	if (s1 == NULL || s2 == NULL)
+		_putchar(' ');
+
+	nstr = malloc(ln3);
+	if (nstr == NULL)
+		return (NULL);
+
+	for (i = 0; i < ln1; i++)
+	{
+		nstr[i] = s1[i];
+	}
+	for (j = 0; j < ln2; j++)
+	{
+		nstr[i + j] = s2[j];
+	}
+	nstr[ln3] = '\0';
+	return (nstr);
+}
