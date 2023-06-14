76132f551f1b78f35679016bb4a3823daf70d03b
Amiyahayadev <ameenahyahayab@gmail.com>
2023-06-14 09:45:49 -0700

    function definition for strdup()

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,61 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - creates duplicate of a string in a newly alloacted memory
+ * @str: pointer to original string
+ * Return: pointer to deplicate string
+ */
+char *_strdup(char *str)
+{
+	char *dstr;
+
+	unsigned int ln = _strlen(str) + 1;
+
+	if (str == NULL)
+		return (NULL);
+	dstr = malloc(sizeof(char) * ln);
+
+	if (dstr == NULL)
+		return (NULL);
+
+	dstr = _memcpy(dstr, str, ln);
+	return (dstr);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string to be measured
+ * Return: the actual length of the string
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+
+/**
+ * _memcpy - copies n characters from memory area src to memory area dest
+ * @dest: pointer to the destination memory area
+ * where the content is to be copied.
+ * @src: pointer to the source of data to be copied
+ * @n: the number of bytes to be copied
+ * Return: a pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,2 +1,6 @@
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+int _strlen(char *s);
+char *_memcpy(char *dest, char *src, unsigned int n);
+
