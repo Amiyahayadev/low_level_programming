58dd41d6bc375fdf9cfe1e43af15a6d1dade5e03
Amiyahayadev <ameenahyahayab@gmail.com>
2023-06-14 12:13:17 -0700

    added funtion prototypes

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,4 +3,8 @@ char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 int _strlen(char *s);
 char *_memcpy(char *dest, char *src, unsigned int n);
-
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
