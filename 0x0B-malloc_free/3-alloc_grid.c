5f63044eb552027f88095bd91929900a09838eb7
Amiyahayadev <ameenahyahayab@gmail.com>
2023-06-15 02:45:16 -0700

    update code to free all previously allocated mem

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,7 +25,7 @@ int **alloc_grid(int width, int height)
 		tdarr[i] = malloc(width * sizeof(int));
 		if (tdarr[i] == NULL)
 		{
-			for(j = 0; j < i; j++)
+			for (j = 0; j < i; j++)
 			{
 				free(tdarr[j]);
 			}
