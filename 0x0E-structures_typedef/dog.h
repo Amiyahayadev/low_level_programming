9bb7e1fced21af741e8735ca775f3bee69aeaca4
Amiyahayadev <ameenahyahayab@gmail.com>
2023-06-19 10:43:05 -0700

    added struct Documentation

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - user defined type for dogs
+ * @name: member 1 is pointer to name string
+ * @age: member 2 is age of float type
+ * @owner: member 3 is pointer to owner string
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif /* DOG_H already exists */
