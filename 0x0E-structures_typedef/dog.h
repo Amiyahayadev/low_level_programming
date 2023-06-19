f19648a0da00da982a31520ed950442a65bcba09
Amiyahayadev <ameenahyahayab@gmail.com>
2023-06-19 13:42:18 -0700

    added type definition for struc dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,16 +7,17 @@
  * @age: member 2 is age of float type
  * @owner: member 3 is pointer to owner string
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-/*dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);*/
+/*dog_t *new_dog(char *name, float age, char *owner);*/
+/* void free_dog(dog_t *d);*/
 int _strlen(char *s);
 char *_strcpy(char *dest, char *src);
 
