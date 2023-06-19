2908b0d678aa9826de8cc9264e4c1f2d58a9847e
Ms Amina Yahaya Baba <ameenahyahayab@gmail.com>
2023-06-19 22:19:13 +0100

    Update when pointer to struct is null for print_dog()

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,22 +11,21 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 	{
-		printf("\n");
-		exit(98);
+		return;
 	}
 
 	if (d->name != NULL)
 		 printf("Name: %s\n", d->name);
 	else
-		printf("Name: (nil)");
+		printf("Name: (nil)\n");
 
-	if (d->age != 0)
+	if (d->age != 0.000000)
 		printf("Age: %f\n", d->age);
 	else
-		printf("Age: (nil)");
+		printf("Age: (nil)\n");
 
 	if (d->owner != NULL)
 		 printf("Owner: %s\n", d->owner);
 	else
-		printf("Owner: (nil)");
+		printf("Owner: (nil)\n");
 }
