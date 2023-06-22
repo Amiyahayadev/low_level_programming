67aa81c362d3536abf7689e409c5e805781266ca
Amiyahayadev <ameenahyahayab@gmail.com>
2023-06-22 13:50:27 -0700

    Created files and directories

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef VA_FUNCTIONS_H
+#define VA_FUNCTIONS_H
+
+#include <stdarg.h>
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+int _putchar(char c);
+
+#endif /* VA_FUNCTIONS_H exists*/
