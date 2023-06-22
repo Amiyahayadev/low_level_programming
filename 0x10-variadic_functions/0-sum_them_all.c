645d25fcddc82bbf9cb2172ecf041271290c94f3
Amiyahayadev <ameenahyahayab@gmail.com>
2023-06-22 14:05:02 -0700

    created variadic funciton that sums arguments

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sums all arguments
+ * @n: number of additional arguments
+ * Return: total sum on SUCCESS or 0
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int t_sum;
+
+	unsigned int i;
+
+	va_list ap;
+
+	va_start(ap, n);
+
+	t_sum = 0;
+	for (i = 0; i < n; i++)
+	{
+		t_sum += va_arg(ap, int);
+	}
+	va_end(ap);
+
+	return (t_sum);
+}
