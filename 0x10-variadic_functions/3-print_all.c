e42088aaeb25ab46ac379a518d8008238dc10a9b
Amiyahayadev <ameenahyahayab@gmail.com>
2023-06-24 05:15:01 -0700

    removed excess declaration in functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,53 +1,6 @@
 #include "variadic_functions.h"
 
 /**
- * print_char - prints an argument as a character
- * @arg: pointer to variadic func argument
- * Return: nothing
- */
-void print_char(va_list arg)
-{
-	char c = va_arg(arg, int);
-	printf("%c", c);
-}
-
-/**
- * print_int - prints an argument as an integer
- * @arg: pointer to variadic func argument
- * Return: nothing
- */
-void print_int(va_list arg)
-{
-	printf("%d", va_arg(arg, int));
-}
-
-/**
- * print_float - prints an argument as a float
- * @arg: pointer to variadic func argument
- * Return: nothing
- */
-void print_float(va_list arg)
-{
-	float f = va_arg(arg, double);
-
-	printf("%f", f);
-}
-
-/**
- * print_string -prints an argument as a string
- * @arg: pointer to variadic func argument
- * Return: noting
- */
-void print_string(va_list arg)
-{
-	char *s = va_arg(arg, char *);
-
-	if (s == NULL)
-		s = "(nil)";
-	printf("%s", s);
-}
-
-/**
  * print_all - function that prints anything
  * Description: parameter type is constant pointer to a constant character
  * @format: a list of types of arguments passed to the function
@@ -89,3 +42,52 @@ void print_all(const char * const format, ...)
 	va_end(ap);
 	printf("\n");
 }
+
+/**
+ * print_char - prints an argument as a character
+ * @ap: pointer to variadic func argument
+ * Return: nothing
+ */
+void print_char(va_list ap)
+{
+	char c = va_arg(ap, int);
+
+	printf("%c", c);
+}
+
+/**
+ * print_int - prints an argument as an integer
+ * @ap: pointer to variadic func argument
+ * Return: nothing
+ */
+void print_int(va_list ap)
+{
+	printf("%d", va_arg(ap, int));
+}
+
+/**
+ * print_float - prints an argument as a float
+ * @ap: pointer to variadic func argument
+ * Return: nothing
+ */
+void print_float(va_list ap)
+{
+	float f = va_arg(ap, double);
+
+	printf("%f", f);
+}
+
+/**
+ * print_string -prints an argument as a string
+ * @ap: pointer to variadic func argument
+ * Return: noting
+ */
+void print_string(va_list ap)
+{
+	char *s = va_arg(ap, char *);
+
+	if (s == NULL)
+		s = "(nil)";
+	printf("%s", s);
+}
+
