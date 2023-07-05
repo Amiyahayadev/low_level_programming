1b91297eb3f8376c8cd7311aa9ff033d581ce48a
Amiyahayadev <ameenahyahayab@gmail.com>
2023-07-05 09:09:45 -0700

    frees list and sets reference to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a list of integer data and sets it to NULL
+ * @head: Indirect reference to the SLL
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *nextFree;
+
+	if (head != NULL && *head != NULL)
+	{
+		while (*head != NULL)
+		{
+			nextFree = (*head)->next;
+			free(*head);
+			*head =  nextFree;
+		}
+		*head = NULL;
+	}
+}
