e7b797bc1997537798589fc6e21a8017793932c3
Amiyahayadev <ameenahyahayab@gmail.com>
2023-07-07 15:38:58 -0700

    print binary recursively

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,8 +9,8 @@ void print_binary(unsigned long int n)
 {
 	/*Print a single '0' if the number is zero*/
 	if (n > 1)
-	{ 
+	{
 		print_binary(n >> 1);
 	}
-	_putchar((n & 1) + '0');	
+	_putchar((n & 1) + '0');
 }
