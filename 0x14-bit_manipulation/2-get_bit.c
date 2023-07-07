d42eba09513bda798c6e60df9b1415407414c495
Amiyahayadev <ameenahyahayab@gmail.com>
2023-07-07 15:23:01 -0700

    gets bit at index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_bit - function that gets a bit at a given index
+ * @n: unsigned long int decimal
+ * @index: the index, starting from 0
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int m;
+
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+
+	m = 1UL << index; /*container to hold long int*/
+	return (n & m) != 0;
+}
