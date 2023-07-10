cfbdbddf3ecf99b993df0a06bfe044ddc634d788
Amiyahayadev <ameenahyahayab@gmail.com>
2023-07-10 16:53:58 -0700

    function that reads

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -30,10 +30,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	buff[r] = '\0';
 	n = write(STDOUT_FILENO, buff, r);
 	if (n == -1 || !r)
+	{
+		free(buff);
+		close(fd);
 		return (0);
-	else
-		return (n);
-
+	}
 	free(buff);
 	close(fd);
+	return (n);
 }
