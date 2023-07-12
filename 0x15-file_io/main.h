66fd4e161030a34a5127535ca5d2b02512c78089
Amiyahayadev <ameenahyahayab@gmail.com>
2023-07-11 17:18:53 -0700

    cLibrary

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,7 +12,8 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void print_error(int error_code, const char *file_name);
 int cp(const char *file_from, const char *file_to);
-void copy_file_contents(int fd_from, int fd_to, const char *file_to, const char *file_from);
+void copy_file_contents(int fd_from, int fd_to, const char *file_to,
+const char *file_from);
 int _strlen(char *s);
 
 #endif
