3b349bad7511a3fe3600230524b3dd1f886e4678
Amiyahayadev <ameenahyahayab@gmail.com>
2023-08-03 16:43:21 -0700

    bitwise operations to set/clear a bit at index

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * clear_bit - sets the value of a bit to 0 at a given index
+ * @n: pointer to unsigned long int decimal
+ * @index: the index, starting from 0
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int m;
+
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+
+	m = 1UL << index;
+
+	/*Clear the bit at the specified index to 0*/
+	*n &= ~m;
+	return (1);
+}
