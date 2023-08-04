f308bc0eb482359ed0a44b3f418822731100f5ac
Amiyahayadev <ameenahyahayab@gmail.com>
2023-08-03 17:11:52 -0700

    function that checks byte-order

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * get_endianness - function that checks the endianness
+ *
+ * Return: 0 for big endian, 1 for little endian
+ */
+int get_endianness(void)
+{
+	unsigned int x = 1;
+
+	char *c = (char *) &x;
+
+	return ((int) *c);
+}
